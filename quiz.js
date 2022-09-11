f279d6030450dd7fa688a4c398aff5cbae8ac561
Purva Rajyaguru <rajyagurupurva28@gmail.com>
2022-09-11 10:25:54 +0530

    solved some issues

---
+++ quiz.js
@@ -0,0 +1,23 @@
+const formQuiz = document.querySelector(".quizform");
+const btn = document.querySelector("#btn");
+const output = document.querySelector("#output");
+
+const answers=['90','right angle'];
+
+function calculateScore(){
+    let score = 0;
+    let index = 0;
+    const formResult = new FormData(formQuiz);
+    for (let value of formResult.values()){
+        //console.log(value);
+        if(value === answers[index]){
+            score=score+1;
+        }
+        index=index+1;
+    }
+    //console.log(score);
+    output.innerText = "your score is " + score;
+}
+
+
+btn.addEventListener("click",calculateScore);
